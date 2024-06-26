16a2f50aab130ba609107acabd51e00bb8f39579
Phạm Minh Đức <minhduc5116@gmail.com>
2024-06-27 03:18:28 +0700

    fix feedback

--- ui.js
+++ ui.js
@@ -14,5 +14,30 @@ animation.forEach(ani => {
 
 
 const initSlider = () => {
-    const feedBack = document.querySelector("")
+    const feedBackList = document.querySelector("#feedback-list");
+    const slideButtons = document.querySelectorAll(".slide-button");
+    const maxScrollLeft = feedBackList.scrollWidth - feedBackList.clientWidth;
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "pre-slide" ? -1 : 1;
+            const scrollAmount = feedBackList.offsetWidth * direction;
+            feedBackList.scrollBy({left: scrollAmount, behavior: "smooth"});
+        })
+    })
+
+    
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = feedBackList.scrollLeft <= 0 ? "none" : "block";
+        slideButtons[1].style.display = feedBackList.scrollLeft >= maxScrollLeft ? "none" : "block"
+    }
+    feedBackList.addEventListener("scroll",() => {
+    handleSlideButtons()
+    })
 }
+
+
+
+
+
+window.addEventListener("load", initSlider);
